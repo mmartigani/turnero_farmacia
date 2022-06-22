3b3acedf4a0387b9f5bf60bc63cf3b552c1acd46
ZeroCool <cafefanatico@hotmail.com>
2022-06-22 10:02:31 -0300

    agrego codigo

---
+++ numeros.py
@@ -0,0 +1,30 @@
+def numeros_perfumeria():
+    for n in range(1,10000):
+        yield f'P-{n}'
+
+def numeros_farmacia():
+    for n in range(1,10000):
+        yield f'F-{n}'
+
+def numeros_cosmetica():
+    for n in range(1,10000):
+        yield f'F-{n}'
+
+p=numeros_perfumeria()
+f=numeros_farmacia()
+c=numeros_cosmetica()
+
+def decorador(rubro):
+    
+    print('\n' + '*' *23)
+    print('su numero es: ')
+    if rubro =='P':
+        print(next(p))
+    elif rubro=='F':
+        print(next(f))
+    else:
+        print(next(c))
+    print('aguarde y sera atentido')
+    print('*' *23 + '\n')
+    
+    
